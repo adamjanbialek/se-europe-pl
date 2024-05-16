65d810de1b238ad81f59133116a069529b0ba21b
adambialek86 <adamjanbialek@gmail.com>
2024-05-16 23:37:17 +0200

    sidebar i poczatek pracy nad tabelkami - koniec na dzisiaj (4h, z tego 2h walki z digital ocean )

--- src/components/SidebarComponent/SidebarComponent.js
+++ src/components/SidebarComponent/SidebarComponent.js
@@ -1,9 +1,46 @@
 import './SidebarComponent.scss'
+import {useContext} from "react";
+import {Context} from "../../App";
+import {Link} from "react-router-dom";
+import {HomePage} from "../../pages/HomePage/HomePage";
+import {MyCoupling} from "../../pages/MyCoupling/MyCoupling";
+import {MyMachine} from "../../pages/MyMachine/MyMachine";
+import {AboutUs} from "../../pages/AboutUs/AboutUs";
+import settings from '../../assets/icons/settings.png'
+import user from '../../assets/icons/user.png'
 
 export const SidebarComponent = () => {
+    const [toggleSidebar, setToggleSidebar] = useContext(Context);
+
     return (
-        <aside>
-            <h2 className={'section-title'}>Menu</h2>
+        <aside className={`${!toggleSidebar ? 'aside-container--hidden' : ''}`}>
+            <div className={'aside__line-container'}>
+                <h2 className={'section-subtitle'}>
+                    Menu
+                </h2>
+                <button className={'btn'} onClick={() => {setToggleSidebar(false)}}>x</button>
+            </div>
+            <div className={'aside__line-container'}>
+                <picture className={'sidebar-icon'}>
+                    <img src={user} />
+                </picture>
+                <p>Sign in</p>
+            </div>
+            <div className={'aside__line-container'}>
+                <picture className={'sidebar-icon'}>
+                    <img src={settings} />
+                </picture>
+                <p>Settings</p>
+            </div>
+            <div className={'aside__line-container aside__line-container--column'}>
+                <Link to={'moje-zlacze'} element={<MyCoupling/>}>Moje złącze</Link>
+                <Link to={'/moja-maszyna'} element={<MyMachine/>}>Moja maszyna</Link>
+                <Link to={'/o-nas'} element={<AboutUs/>}>O nas</Link>
+            </div>
+            <div className={'aside__line-container aside__line-container--column'}>
+                <Link to={'/'} element={<HomePage />}>Start</Link>
+                <Link to={'/'} >Kontakt</Link>
+            </div>
         </aside>
     );
 }
---
+++ src/components/TableComponent/TableComponent.js
@@ -0,0 +1,26 @@
+export const TableComponent = () => {
+    return (
+        <main>
+            <div>
+                /* TODO: zaimplementować material-ui */
+                <div className={''}>
+                    <div>
+                        Waga maszyny
+                    </div>
+
+                </div>
+                <div className={''}>
+                    <div>
+                        Złączę
+                    </div>
+                    <input />
+                    <input />
+                    <input />
+                    <input />
+                    <input />
+                    <input />
+                </div>
+            </div>
+        </main>
+    );
+}
