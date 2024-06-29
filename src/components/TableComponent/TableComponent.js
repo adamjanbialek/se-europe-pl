ed094f949251fce940f5b8162b22039835d7dabf
adambialek86 <adamjanbialek@gmail.com>
2024-06-29 18:04:11 +0200

    przerwa

--- src/components/TableComponent/TableComponent.js
+++ src/components/TableComponent/TableComponent.js
@@ -11,11 +11,38 @@ import {Link} from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import {useContext} from "react";
-import {CartContext} from "../../App";
 
+import {CartContext} from "../../App";
+import jsonProducts from '../../data/products.json';
+
+let products = JSON.parse(JSON.stringify(jsonProducts, null, 2));
 
 export const TableComponent = (props) => {
     const [cart, setCart] = useContext(CartContext);
+
+    function findProduct(product) {
+        const [item] = products.filter(el => el.id === product.artNo);
+        return item;
+    }
+
+    function checkItemQuantity(item, amountToChange) {
+        console.log(cart.find(el => el.id === item.id)?.hasOwnProperty('quantity') ? cart.find(el => el.id === item.id)['quantity'] += amountToChange : item['quantity'] = amountToChange) ;
+    }
+
+    function addToCart() {
+
+    }
+
+    function removeFromCart() {
+
+    }
+
+    function changeCart(product, amountToChange) {
+        const cartProduct = findProduct(product);
+        console.log(amountToChange);
+        checkItemQuantity(cartProduct, amountToChange);
+        setCart([...cart, cartProduct]);
+    }
 
     function isChecked(product) {
         if(Object.entries(props.checkboxes).filter(checkbox => checkbox[1] === true).length === 0 ||
@@ -48,14 +75,14 @@ export const TableComponent = (props) => {
                     {props.data.filter(product=> {
                         return product.weight >= props.displayedItems[0] & product.weight <= props.displayedItems[1]
                     }).filter(product => isChecked(product)).map(product => {
-                        const fields = Object.values(product) ;
+                        const fields = Object.values(product);
                 
                         return (
                             <TableRow
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            ><button className='buy-btn btn-container' onClick={() => {
-                                setCart([...cart, product]);
-                            }}>BUY</button>
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }} onClick={() => {
+                                    console.log(cart);
+                            }}
+                            ><button className='buy-btn btn-container' onClick={() => changeCart(product, 1)}>BUY</button>
                             <FontAwesomeIcon icon={faHeart} className={'like favourite-container'} onClick={onToggleFav} />
                                 {fields.map((field, index) => {
                                     return index === 0 ? <TableCell><Link to={`../${field}`}>{field}</Link></TableCell> : <TableCell>{field}</TableCell>;
